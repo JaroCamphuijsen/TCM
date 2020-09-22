2829c03295566b8bfb5f9ef7f05d2fcd24746157
mchertova <m.chertova@esciencecenter.nl>
2020-09-22 09:13:22 +0200

    added more flexible cyclone placing + custom made offset between new and old cyclone based on distance beetween their start and end point

--- visualisation.py
+++ visualisation.py
@@ -6,13 +6,15 @@ from statistics import mean
 from omuse.ext.spherical_geometry import distance
 from omuse.units import units
 
-filename = sys.argv[1]  # input file with list of cyclones
+filename = sys.argv[1]  #  input file
 start_line = int(sys.argv[2]) # line to start reading cyclones
 multiple_basins = False
-effective_distance = 1500 | units.km 
+effective_distance = 1500.0  
 time_offset = 2*8 #  8 is one day
-pre_selection = True
+pre_selection = False
 Ncyclones = 0
+order = 0
+radius= 6371 
 cyclones=dict()
 
 class Cyclone(object):
@@ -35,6 +37,7 @@ class Cyclone(object):
     self.distance = []  # [min distance to another cyclones in km]
     self.connection = []  # 0 - cyclones can not be placed together, 1 - can 
     self.shift = []  # if > 0: shift(each step is 3 h)  at which cyclones can be placed together
+    self.order = 0
 
   def set_times(self, times):
     self.times.append(times)
@@ -58,7 +61,7 @@ class Cyclone(object):
     self.pressure.append(pressure)
 
 
-def read_data(filename, cyclones, multiple_basins, pre_selection):
+def read_data(filename, cyclones, multiple_basins,pre_selection):
   inp_file = open(filename) 
   index_global = -1
   index_old = -1
@@ -78,8 +81,8 @@ def read_data(filename, cyclones, multiple_basins, pre_selection):
         cyclones[index_global]=Cyclone(yr,index)
         cyclones[index_global].basin = int(float(fields[4]))
         cyclones[index_global].month = int(float(fields[1]))
-      # For fast computation exclude cyclones without landfall, 
-      # set their status to finished
+
+      # Exclude cyclones without landfall, 
         if pre_selection == True:
           if index_global  > 0:
             if cyclones[index_global-1].landfall == 0:
@@ -111,7 +114,7 @@ def read_data(filename, cyclones, multiple_basins, pre_selection):
             cyclones[index_global].landfall = int(float(fields[11])) 
 
       # Stop counting and print line number for next iteration  
-      if (index_global - n_unactive) ==300:
+      if (index_global - n_unactive) == 300:
         break
 
   Ncyclones = index_global
@@ -131,14 +134,13 @@ def read_data(filename, cyclones, multiple_basins, pre_selection):
           if cyclones[i].basin == bas:
             tot_cyclones[bas] += 1
             tot_tsteps[bas] +=  max(cyclones[i].times)
-    print ("Tot N of cyclones per basin",tot_cyclones,"Tot N of tsteps", tot_tsteps)  
+
   return Ncyclones
 
 
 def set_distances(Ncyclones, cyclones):
   lat_set={}
   lon_set={}
-  radius= 6371 | units.km
 
   #   cyclone[1]:
   #               lat(1)   lon(1)
@@ -158,28 +160,44 @@ def set_distances(Ncyclones, cyclones):
   #	          lat(len(cyclones[last].lat))   lon(len(cyclones[last].lat))  
   
   for i in range(0, Ncyclones):
-    lat_set[i] = numpy.array(cyclones[i].lats) | units.deg
-    lon_set[i] = numpy.array(cyclones[i].lons) | units.deg
+    lat_set[i] = numpy.array(cyclones[i].lats) 
+    lon_set[i] = numpy.array(cyclones[i].lons) 
   for i in range(0, Ncyclones): 
     for j in range(0, Ncyclones):
-      dist = 0. | units.km
-      dist_iter = [] 
+      dist = 0. 
+      dist_iter = []
       k = 0
       shift = -1
       connection = 0
+      track_length = 0
       if cyclones[j].basin == cyclones[i].basin:      
-        if cyclones[i].status == -1 and cyclones[j].status == -1:
-          for lat, lon in zip(lat_set[i], lon_set[i]):
-            dist_iter.append(min(distance(lat, lon, lat_set[j], lon_set[j], \
-                             R=radius).in_(units.km)))
-            k = k + 1 
-        
+        if i != j and cyclones[i].status == -1 and cyclones[j].status == -1:
+
+          for lat1, lon1 in zip(lat_set[i], lon_set[i]):
+            dist_single = []
+            lat2 = lat_set[j]
+            lon2 = lon_set[j] 
+
+            if len(lat_set[j]) >= len(lat_set[i]) - track_length:
+              npoints = len(lat_set[i]) - track_length
+            else:
+              npoints = len(lat_set[j])
+
+            for m in range(0, npoints):
+              dist_single.append(distance(lat1, lon1, lat2[m], lon2[m],  \
+                                 R=radius))
+
+            dist_iter.append(min(dist_single))
+            k = k + 1
+            track_length += 1 
+
           if max(dist_iter) < effective_distance: 
             dist = min(dist_iter)
             shift = -1
             connection = 0
           else:
             l = 0
+
             for l in reversed(range(0, k-1)):
               dist_temp=min(dist_iter[l:k-1]) 
               if (dist_temp) >= effective_distance:
@@ -188,12 +206,14 @@ def set_distances(Ncyclones, cyclones):
                 connection = 1
               if shift > 0 and dist_temp <= effective_distance:
                 break
+
         else:
           connection = 0
           shift = -1
-          dist = 0. | units.km
+          dist = 0.0 
+
       else:
-        dist = 1e5 | units.km
+        dist = 1e5 
         shift = 0
         connection = 1
 
@@ -201,8 +221,9 @@ def set_distances(Ncyclones, cyclones):
       cyclones[i].shift.append(shift)
       cyclones[i].distance.append(dist)
 
-# Select from the list all events which do not intersect with each other at time 0
+# Select from the list all events which do not intersect at time 0
 def place_events_initial(Ncyclones, effective_distance, cyclones):
+  global order
   first_cyclone = True
 
   for i in range(0, Ncyclones):
@@ -223,50 +244,78 @@ def place_events_initial(Ncyclones, effective_distance, cyclones):
           min_distance.append(cyclones[i].distance[j])
         elif cyclones[j].status == 0 and (cyclones[i].connection[j] != 1 \
            or cyclones[i].shift[j] != 0):
-          min_distance.append(0 | units.km)
+          min_distance.append(0)
 
       if len(min_distance) > 0 and min(min_distance) > effective_distance:
         cyclones[i].status = 0
         cyclones[i].start_time = 0
+        order += 1
+        cyclones[i].order = order
 
-# Place cyclone to replace finished one with spinup time offset 
-def place_event(Ncyclones, effective_distance, cyclones, current_time):
+# Place cyclone to replace finished  
+def place_event(Ncyclones, effective_distance, cyclones, current_time,time_offset):
+  global order
   week = 56
 
+  placed_cyclone = False
   for i in range(0, Ncyclones):
     min_distance = []
+
     if cyclones[i].status == -1:
       active_cyclone = False
-
+      coun = 0
       for j in range(0, Ncyclones):
         if cyclones[j].status == 0:  
           active_cyclone = True
+          coun += 1
           if (cyclones[j].connection[i] == 1 and       \
-              cyclones[j].start_time < current_time):
+              cyclones[j].start_time <= current_time):
  
-            if (cyclones[i].shift[j] > (current_time - cyclones[j].start_time) \
+            if (cyclones[j].shift[i] == (current_time - cyclones[j].start_time) \
                 and cyclones[j].start_time < current_time):
-                min_distance.append(cyclones[i].distance[j])
-            elif cyclones[i].shift[j] == 0:
-              min_distance.append(cyclones[i].distance[j])
+                min_distance.append(cyclones[j].distance[i])
+            elif cyclones[j].shift[i] == 0:
+              min_distance.append(cyclones[j].distance[i])
             else:            
-              min_distance.append(0 | units.km)
-
-          elif (cyclones[j].connection[i] == 1 and cyclones[i].shift[j] == 0 and \
-                cyclones[j].start_time >= current_time):
-            min_distance.append(cyclones[i].distance[j])
+              min_distance.append(0)
 
           else:            
-             min_distance.append(0 | units.km)
-   
+             min_distance.append(0)
       # At the end we always have a N of large cyclones which can only be placed alone 
       if not active_cyclone:
         cyclones[i].status = 0
         cyclones[i].start_time = current_time + time_offset
+        order += 1
+        cyclones[i].order = order
+        placed_cyclone = True       
+    if placed_cyclone == True:
+      break    
+    else:
+      if len(min_distance) > 0 and min(min_distance) > effective_distance:
+        cyclones[i].status = 0
+        order += 1
+        cyclones[i].order = order
+     
+        # Added custom made offset (time_offset2)
+        # time_offset is regular one
+        dist_min = []
+        dist_min.append(effective_distance)
 
-    if len(min_distance) > 0 and min(min_distance) > effective_distance:
-      cyclones[i].status = 0
-      cyclones[i].start_time = current_time + time_offset
+        for j in range(0, Ncyclones):
+          if (current_time-8 <= cyclones[j].start_time+max(cyclones[j].times) <= current_time):
+            lat2 = cyclones[j].lats[len(cyclones[j].lats)-1] 
+            lon2 = cyclones[j].lons[len(cyclones[j].lons)-1] 
+            dist_min.append(distance(cyclones[i].lats[0],cyclones[i].lons[0], \
+                            lat2, lon2, R=radius))  
+
+        if min(dist_min) >= effective_distance:
+          time_offset2 = time_offset
+        else:
+          time_offset2 = int(8*(1-min(dist_min)/effective_distance))
+          
+        cyclones[i].start_time = current_time + time_offset2
+        #cyclones[i].start_time = current_time + time_offset
+        placed_cyclone = True
 
 
 # Plotting and writing data
@@ -327,31 +376,30 @@ def tstepping(Ncyclones, cyclones):
             line_col = "purple"
           plt.plot(cyclones[i].lons, cyclones[i].lats, transform=ccrs.PlateCarree(), \
                  color=line_col)
-          plt.text(mean(cyclones[i].lons)+1, mean(cyclones[i].lats), i, \
-                 transform=ccrs.PlateCarree(),color=line_col)
+          plt.text(mean(cyclones[i].lons)+1, mean(cyclones[i].lats),     \
+                   cyclones[i].start_time, transform=ccrs.PlateCarree(), \
+                   color=line_col)
 
-    # Creating 1 output in 3 days 
+    # 1 output in 3 days 
     if current_time % 24 ==0:  
       count = int(current_time/24)
       name = "test_"+str(count)+".png"
       plt.savefig(name, format='png')
 
-      # Extras for plotting
-      #ax.set_extent([-150,-10,-5,40])
       plt.pause(0.002)
       plt.clf()
       ax = plt.axes(projection=ccrs.PlateCarree())
       ax.stock_img()
 
     #------ FIND NEW CYCLONE------
-    place_event(Ncyclones, effective_distance, cyclones, current_time)
+    place_event(Ncyclones, effective_distance, cyclones, current_time, time_offset)
 
   plt.show()  
 
       
 if __name__=="__main__":
   Ncyclones = read_data(filename, cyclones, multiple_basins, pre_selection)
-  Ncyclones = 50 # for fast check
+  Ncyclones = 50
   set_distances(Ncyclones, cyclones) 
   place_events_initial(Ncyclones, effective_distance, cyclones)
   tstepping(Ncyclones, cyclones)
