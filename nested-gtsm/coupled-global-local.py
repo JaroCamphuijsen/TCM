e02aa6b0c4e5aa85c5d520b557c0bc6e10070ce2
Jaro Camphuijsen <jjecamphuijsen@gmail.com>
2023-12-19 11:35:53 +0100

    add function for automated forcing file generation (pli and cmp)

--- nested-gtsm/coupled-global-local.py
+++ nested-gtsm/coupled-global-local.py
@@ -1,5 +1,9 @@
 import os
-import numpy
+import numpy as np
+
+import dfm_tools as dfmt  # this requires numpy >= 1.22, which requires the 2022 software stack on snellius
+import hydrolib.core.dflowfm as hcdfm
+import xarray as xr
 
 from datetime import datetime
 
@@ -10,6 +14,7 @@ from matplotlib import pyplot
 
 start_date="20110130"
 start_datetime=datetime.strptime(start_date,"%Y%m%d")
+local_bbox = [] # lon_min, lon_max, lat_min, lat_max
 tend=4.5 | units.day
 dt=(1.| units.hour)
 
@@ -25,9 +30,68 @@ def init_global(start_date):
 
     return d
 
-def init_nested(start_date):
-    d=DFlowFM(ini_file="RF.mdu", coordinates="spherical", redirection="none", workdir="nested")
- 
+
+def create_forcing_files(model_name, bbox, course_res, fine_res, bath_file = None, crs = 'EPSG:4326'):
+    """
+    model_name: str,
+    bbox: list, [lon_min, lon_max, lat_min, lat_max]
+    course_res: float, course resolution of the grid in degrees 
+    fine_res: float, minimal edge length of the refined grid in meters
+    bath_file: str, path to the bathymetry file, default: None 
+    crs: str, coordinate reference system identifier, default: 'EPSG:4326'
+    """
+    ## use workdir as model_name and create directories
+    dir_output = os.path.abspath(f'./{model_name}')
+    os.makedirs(dir_output, exist_ok=True)
+
+
+    ## from bounding box define the grid extent
+    lon_min, lon_max, lat_min, lat_max = bbox
+    mk_object = dfmt.make_basegrid(lon_min, lon_max, lat_min, lat_max, dx=course_res, dy=course_res, crs=crs)
+    bnd_gdf = dfmt.generate_bndpli_cutland(mk=mk_object, res='h', buffer=0.01)
+    bnd_gdf['name'] = f'{model_name}_bnd'
+    bnd_gdf_interp = dfmt.interpolate_bndpli(bnd_gdf, res=0.03)
+    
+    pli_polyfile = dfmt.geodataframe_to_PolyFile(bnd_gdf_interp)
+    pli_polyfile.save(os.path.join(dir_output, f'{model_name}.pli'))
+
+    # connect to a coarse version of the GEBCO_2022 dataset on OPeNDAP
+    if not bath_file:
+        bath_file = f'https://opendap.deltares.nl/thredds/dodsC/opendap/deltares/Delft3D/netcdf_example_files/GEBCO_2022/GEBCO_2022_coarsefac08.nc'
+    data_bathy_sel = xr.open_dataset(bath_file).sel(lon=slice(lon_min-1,lon_max+1),lat=slice(lat_min-1,lat_max+1))
+
+    # refine grid and remove land with GSHHS coastlines
+    dfmt.refine_basegrid(mk=mk_object, data_bathy_sel=data_bathy_sel, min_edge_size=fine_res)
+    dfmt.meshkernel_delete_withcoastlines(mk=mk_object, res='h')
+
+    # convert to xugrid
+    xu_grid_uds = dfmt.meshkernel_to_UgridDataset(mk=mk_object, crs=crs)
+    # interpolate bathymetry onto the grid
+    data_bathy_interp = data_bathy_sel.interp(lon=xu_grid_uds.obj.mesh2d_node_x, lat=xu_grid_uds.obj.mesh2d_node_y).reset_coords(['lat','lon'])
+    xu_grid_uds['mesh2d_node_z'] = data_bathy_interp.elevation.clip(max=10)
+
+    # write xugrid grid to netcdf
+    xu_grid_uds.ugrid.to_netcdf(os.path.join(dir_output, f'{model_name}_net.nc'))
+
+    # TODO create the .cmp files from template
+    cmp_file = os.path.join(dir_output, f'{model_name}_0001.cmp')
+    with open(cmp_file, "w") as f:
+        f.write(
+            """* COLUMNN=3\n
+            * COLUMN1=Period (min) or Astronomical Componentname\n
+            * COLUMN2=Amplitude (ISO)\n
+            * COLUMN3=Phase (deg)\n
+            0.0  0.  0.0\n 
+            745.0  .0  0.0"""
+        )
+    return ext_forcing_file
+
+def init_nested(start_date, bbox):
+    model_name = "local"
+    bbox = [] # lon_min, lon_max, lat_min, lat_max
+    d=DFlowFM(ini_file="RF.mdu", coordinates="spherical", redirection="none", workdir="local")
+
+    ext_forcing_file = create_forcing_files(model_name, bbox, 0.05, 300)
     # set parameters
     #~ d.parameters.use_interface_wind=False
     #~ d.parameters.use_interface_patm=False
@@ -136,27 +200,61 @@ def allplots(nodes1, nodes2, nodes1b, n, title=""):
 
     pyplot.close(f)
 
+def get_bounding_subgrid(grid, bbox=None):
+    if bbox:
+        lon_min, lon_max, lat_min, lat_max = bbox
+    else: 
+        lon_min = grid.lon.min()
+
+
+    return grid[indices]
+
+def get_subgrid_from_pli(grid, pli_file):
+    pass
 
 
 def find_matching_subgrid(grid, sub, names=["lon","lat"]):
     indices=[]
     for s in sub:
       d=(getattr(s, names[0])-grid.lon)**2+(getattr(s, names[1])-grid.lat)**2
-      indices.append(numpy.argmin(d.number))
-    indices=numpy.array(indices)
+      indices.append(np.argmin(d.number))
+    indices=np.array(indices)
     return grid[indices]
 
 def evolve_gtsm_nested(gtsm, nested, tend, dt):
 
     # fix loose ends on the state model
-    print(gtsm.flow_nodes)
-    print(nested.flow_nodes)
+    print("global flow_nodes:", gtsm.flow_nodes)
+    print("local flow_nodes:", nested.flow_nodes)
+
+    # generate subset of the nested grid
+    # 
+    # boundary_nodes = get_boundary_nodes_subgrid(nested.flow_nodes, bbox)
+    # print(boundary_nodes)
 
     #~ gtsm_boundary_nodes=find_matching_subgrid(gtsm.flow_nodes, nested.boundary_links_forcing, names=["lon_bndz", "lat_bndz"])
-    gtsm_boundary_nodes=find_matching_subgrid(gtsm.flow_nodes, nested.boundary_nodes, names=["lon", "lat"])
+    print("local boundary_nodes:", nested.boundary_nodes)
+    print("local boundary_nodes latvalues:", nested.boundary_nodes.lat)
+    print("local boundary_nodes lonvalues:", nested.boundary_nodes.lon)
+
+    # Select the nodes in the global model that correspond to the boundary of the local model (defined by the forcing files of the deflt3d model)
+    gtsm_boundary_nodes=find_matching_subgrid(gtsm.flow_nodes, nested.boundary_nodes)
+    
+    # Select the nodes in the local model that are at the boundary (defined by the forcing files of the deflt3d model)
+    nested_boundary_links_forcing=find_matching_subgrid(nested.flow_links_forcing, nested.boundary_nodes)
+    print("global gtsm_boundary_nodes:", gtsm_boundary_nodes)
+    print("local nested_boundary_nodes:", nested_boundary_links_forcing)
+
+    # Select the nodes in the global model that are covered by the local model
     gtsm_nested_nodes=find_matching_subgrid(gtsm.flow_nodes, nested.flow_nodes)
 
+    print("global local_flow_nodes:", gtsm_nested_nodes)
+
     channel=gtsm_boundary_nodes.new_channel_to(nested.boundary_links_forcing)
+
+    # channel=gtsm_boundary_nodes.new_channel_to(nested_boundary_nodes) # testing the local linking with a subgrid of nested.flow_nodes
+
+    print("nested boundary_links_forcing:", nested.boundary_links_forcing)
 
     n=0
     while gtsm.model_time < tend-dt/2:
